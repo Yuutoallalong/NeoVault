07027cbb923664783f93d4c52f159b1042173c9f
Jirat Charoenkaew <jirat8098@gmail.com>
2025-02-27 14:28:20 +0700

    Adjust elevatedButton style and add register path

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:my_app/screens/home.dart';
 import 'package:my_app/screens/login.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:my_app/screens/register.dart';
 
 void main() {
   runApp(ProviderScope(child: const MyApp()));
@@ -17,23 +18,32 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData.light().copyWith(
-          textTheme: GoogleFonts.urbanistTextTheme(Theme.of(context).textTheme),
-          primaryColor: const Color(0xFF2A5D97),
-          colorScheme: ColorScheme.fromSeed(
-              seedColor: Colors.white,
-              primary: const Color(0xFF8391A1),
-              secondary: const Color(0xFFE8ECF4)),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.black,
-                  foregroundColor: Colors.white,
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 150, vertical: 22),
-                  textStyle: const TextStyle(fontWeight: FontWeight.bold),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12))))),
+        textTheme: GoogleFonts.urbanistTextTheme(Theme.of(context).textTheme),
+        primaryColor: const Color(0xFF2A5D97),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.white,
+            primary: const Color(0xFF8391A1),
+            secondary: const Color(0xFFE8ECF4)),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.black,
+            foregroundColor: Colors.white,
+            padding: const EdgeInsets.symmetric(vertical: 22),
+            textStyle: const TextStyle(fontWeight: FontWeight.bold),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            minimumSize: Size(double.infinity, 50),
+          ),
+        ),
+        appBarTheme: AppBarTheme(backgroundColor: Colors.transparent),
+      ),
       initialRoute: '/',
-      routes: {'/': (context) => Home(), '/login': (context) => Login()},
+      routes: {
+        '/': (context) => Home(),
+        '/login': (context) => Login(),
+        '/register': (context) => Register()
+      },
     );
   }
 }
