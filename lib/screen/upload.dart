9441e7243c6601303ee72d93ce6eff512f477802
Jirat Charoenkaew <jirat8098@gmail.com>
2025-03-04 19:23:00 +0700

    Adjust upload screen style

--- lib/screen/upload.dart
+++ lib/screen/upload.dart
@@ -9,94 +9,198 @@ class Upload extends StatefulWidget {
 }
 
 class _UploadState extends State<Upload> {
+  bool switchStatus = false;
+  final messageController = TextEditingController();
+  final fromKey = GlobalKey<FormState>();
+  final passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(leading: backLeadingButton(context: context)),
+      appBar: AppBar(
+        leading: backLeadingButton(context: context),
+        title: Text(
+          "Heisenburg",
+          style: TextStyle(fontSize: 34, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 32),
         child: ListView(
           children: [
-            Container(
-              width: 200,
-              height: 100,
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.black, // กำหนดสีของเส้นขอบ
-                  width: 2, // กำหนดความหนาของเส้นขอบ
-                ),
-                borderRadius: BorderRadius.circular(
-                  8,
-                ), // เพิ่มความโค้งให้กับมุมของกรอบ
-              ),
-              child: Column(
-                children: [
-                  Icon(Icons.upload_file_outlined),
-                  Text("Upload here"),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(0xff5992b7),
-                      minimumSize: Size(150, 10),
-                    ),
-                    child: Text("Select file"),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 25),
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Add a message (optional)',
-                hintStyle: TextStyle(color: Color(0xff626272)),
-                filled: true,
-                fillColor: Color(0xfff7f6fb),
-              ),
-            ),
             Column(
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                const SizedBox(
+                  height: 180,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Color(0xFF454545),
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(
+                      8,
+                    ),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.cloud_upload_outlined,
+                        size: 36,
+                      ),
+                      Text(
+                        "Upload here",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600, fontSize: 16),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(vertical: 0),
+                          backgroundColor: Color(0xff5992b7),
+                          minimumSize: Size(140, 40),
+                        ),
+                        child: Text(
+                          "Select file",
+                          style: TextStyle(fontWeight: FontWeight.w400),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 30),
+                TextField(
+                  controller: messageController,
+                  decoration: InputDecoration(
+                      hintText: 'Add a message (optional)',
+                      hintStyle: TextStyle(color: Color(0xff626272)),
+                      filled: true,
+                      fillColor: Color(0xFFF7F6FB),
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(20)),
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 14, horizontal: 16)),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Icon(Icons.access_time, color: Color(0xff626272)),
-                        Text("Expiration date"),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.access_time,
+                              color: Color(0xff626272),
+                              size: 26,
+                            ),
+                            const SizedBox(
+                              width: 12,
+                            ),
+                            Text(
+                              "Expiration date",
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              "7 days",
+                              style: TextStyle(
+                                color: Color(0xff626272),
+                                fontSize: 16,
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 6,
+                            ),
+                            Icon(Icons.edit, color: Color(0xff626272)),
+                          ],
+                        ),
                       ],
                     ),
+                    const SizedBox(
+                      height: 6,
+                    ),
                     Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Text(
-                          "7 days",
-                          style: TextStyle(color: Color(0xff626272)),
+                        Row(
+                          children: [
+                            Icon(Icons.lock, color: Color(0xff626272)),
+                            const SizedBox(
+                              width: 12,
+                            ),
+                            Text("Password",
+                                style: TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.bold)),
+                          ],
                         ),
-                        Icon(Icons.edit, color: Color(0xff626272)),
+                        Transform.scale(
+                          scale: 0.8,
+                          child: Switch.adaptive(
+                            value: switchStatus,
+                            onChanged: (value) {
+                              setState(() {
+                                switchStatus = value;
+                              });
+                            },
+                          ),
+                        ),
                       ],
                     ),
                   ],
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        Icon(Icons.lock, color: Color(0xff626272)),
-                        Text("Password"),
-                      ],
-                    ),
-                    Switch(value: false, onChanged: (value) {}),
-                  ],
+                const SizedBox(
+                  height: 10,
+                ),
+                switchStatus
+                    ? Form(
+                        key: fromKey,
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                              hintText: 'Enter your password',
+                              hintStyle: TextStyle(color: Colors.white),
+                              filled: true,
+                              fillColor: Color(0xff5992b7),
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide.none,
+                                  borderRadius: BorderRadius.circular(12)),
+                              contentPadding: const EdgeInsets.symmetric(
+                                  vertical: 14, horizontal: 16)),
+                          style: TextStyle(color: Colors.white),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Password cannot be empty';
+                            }
+                            return null; // ผ่านการตรวจสอบ
+                          },
+                        ))
+                    : const SizedBox(
+                        height: 0,
+                      ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    ScaffoldMessenger.of(
+                      context,
+                    ).showSnackBar(SnackBar(content: Text('Uploaded')));
+                  },
+                  child: Text('Upload'),
                 ),
               ],
-            ),
-            ElevatedButton(
-              onPressed: () {
-                // You can add functionality for the CONFIRM button here
-                ScaffoldMessenger.of(
-                  context,
-                ).showSnackBar(SnackBar(content: Text('Saved')));
-              },
-              child: Text('Save'),
             ),
           ],
         ),
